f7da006dd5401863876b3a608bc0a30d558b86f6
Dawid Opałka <empo1964@gmail.com>
2026-09-18 10:30:26 +0200

    fix(form): keep the required asterisk next to the label text

--- src/components/form/label/label.tsx
+++ src/components/form/label/label.tsx
@@ -3,13 +3,10 @@ import { cn } from "utils/cn"
 import { LabelProps } from "./label.types"
 
 export const Label = ({ children, className, isRequired, isDisabled, ...props }: LabelProps) => (
-  <label
-    {...props}
-    className={cn("flex gap-1 text-sm font-semibold text-gray-900", isDisabled && "text-gray-500", className)}
-  >
+  <label {...props} className={cn("text-sm font-semibold text-gray-900", isDisabled && "text-gray-500", className)}>
     {children}
     {isRequired && (
-      <span aria-hidden className="text-negative">
+      <span aria-hidden className="ms-1 text-negative">
         *
       </span>
     )}
